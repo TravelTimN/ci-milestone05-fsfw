231981743934d5b396b294ad9a2f9dccb22bfeaf
Tim Nelson <traveltimn@gmail.com>
2019-07-28 19:07:38 +0100

    Accounts App: refactor and cleanup
    Cleaning up with some refactoring before the final submission.
    New background images to match the overall theme.
    Docstrings and Comments also added where applicable.

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from accounts.models import Profile
 
 
+# Register Profile Model to the Admin Panel
 admin.site.register(Profile)
--- project/accounts/backends.py
+++ project/accounts/backends.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import User
 class EmailAuth():
     """ Authenticate a user by an exact match on the email and password """
     def authenticate(self, username=None, password=None):
-        """
-        Get an instance of 'User' based off the email and verify the password
-        """
+        # Get an instance of 'User' based off the email and verify the password
         try:
             user = User.objects.get(email=username)
             if user.check_password(password):
@@ -16,9 +14,7 @@ class EmailAuth():
             return None
 
     def get_user(self, user_id):
-        """
-        Used by the Django authentication system to retrieve a user instance
-        """
+        # Used by the Django authentication system to retrieve a user instance
         try:
             user = User.objects.get(pk=user_id)
             if user.is_active:
